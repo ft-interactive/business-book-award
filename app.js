c22f03741d628c30683abe868951455a33f111ec
Luke Kavanagh <ltkavanagh@gmail.com>
2014-10-22 11:05:30 +0100

    basic app

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('App');
+var http = require('http')
+var lib = require('./lib');
+var app = lib();
+var server = http.createServer(app.callback());
+
+server.listen(process.env.PORT || 3000);
