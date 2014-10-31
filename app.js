d13d606394b23f819b5da854e3e79fadecad6078
Luke Kavanagh <ltkavanagh@gmail.com>
2014-10-31 13:38:04 +0000

    dist build to allow deploying to heroku

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ var http = require('http')
 var lib = require('./lib');
 var pkg = require('./package.json');
 
+var prod = process.env.NODE_ENV === 'production';
+
 var options = {
   name: pkg.name,
   proxy: true,
+  staticBaseUrl: prod ? '/' : 'http://localhost:9000/',
   dummydata: process.env.DUMMY_DATA === 'true',
-  cacheViews: /*'memory'*/process.env.CACHE_VIEWS === 'true'
+  cacheViews: prod ? 'memory' : false
 };
+
+if (prod) {
+  options.theme = {
+    views: '../node_modules/business-books-of-the-decade-frontend/dist/views',
+    public: '../node_modules/business-books-of-the-decade-frontend/dist/public',
+    data: '../node_modules/business-books-of-the-decade-frontend/dist/views/data.js',
+    favicon: './public/favicon.ico',
+    filters: '../node_modules/business-books-of-the-decade-frontend/dist/views/filters.js'
+  };
+} else {
+  options.theme = {
+    views: '../node_modules/business-books-of-the-decade-frontend/.tmp/views',
+    public: '../node_modules/business-books-of-the-decade-frontend/.tmp',
+    data: '../node_modules/business-books-of-the-decade-frontend/.tmp/views/data.js',
+    favicon: './public/favicon.ico',
+    filters: '../node_modules/business-books-of-the-decade-frontend/.tmp/views/filters.js'
+  };
+}
 
 var app = lib(options);
 var server = http.createServer(app.callback());
