aae2868f70370cec61a0c1c68f4cb8aa3790202c
luke.kavanagh <Luke.Kavanagh@ft.com>
2015-08-06 16:24:25 +0100

    www.ft.com doesn't have SSL

--- server/site.js
+++ server/site.js
@@ -3,7 +3,7 @@ var site = {
   baseurl: {
     static: process.env.STATIC_BASE ? process.env.STATIC_BASE : '',
     pages: process.env.PAGES_BASE || '',
-    ft: process.env.FT_HOME || '//www.ft.com/',
+    ft: process.env.FT_HOME || 'http://www.ft.com/',
     api: process.env.API_BASE || ''
   }
 };
