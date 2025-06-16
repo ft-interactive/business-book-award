541a767b72224ceb19d40906929dee06908b73c5
Filip Hristov <filip.hristov@ft.com>
2025-06-16 16:53:18 +0300

    adding auth query parameter check, in order to ensure only user with correct auth token can perform a data refresh, otherwise send a 401 unauthorized message.

--- server/api/refresh-data.js
+++ server/api/refresh-data.js
@@ -1,7 +1,9 @@
 var service = require('../service');
 
 module.exports = function*(next) {
+  console.log('Refresh data request received');
   this.assert(this.query.auth, 404, 'Not found');
+  this.assert(this.query.auth == process.env.SPREADSHEET_REFRESH_TOKEN, 401, 'Invalid auth token');
   var republish = 'republish' in this.query;
   var changed = yield service.refresh(republish);
   var msg = (republish ? 'REPUBLISH' : 'Refresh') + 'ed at ' + (new Date()).toUTCString();
